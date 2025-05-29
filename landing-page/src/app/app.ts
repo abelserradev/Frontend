5c77dd540433f2e14f1c99707a00d8932bbc9a67
Abel Serra <aj_serra@hotmail.com>
2025-05-29 15:23:25 -0400

    cambios en la estructura

--- landing-page/src/app/app.ts
+++ landing-page/src/app/app.ts
@@ -1,17 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
-import { Footer } from './components/footer/footer';
-import { Hero } from './components/hero/hero';
-import { Features } from './components/features/features';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, CommonModule, Footer, Hero, Features],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'El-dorado';
+  protected title = 'landing-page';
 }
