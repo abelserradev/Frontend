6ef9b38914fe87c53f921eaf7b446cf0686cef98
Abel Serra <aj_serra@hotmail.com>
2025-05-29 14:36:33 -0400

    ultimos endpoint para criptomonedas y monedas

--- landing-page/src/app/app.ts
+++ landing-page/src/app/app.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Hero } from './components/hero/hero';
+import { Features } from './components/features/features';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header, CommonModule, Footer, Hero, Features],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'landing-page';
+  protected title = 'El-dorado';
 }
